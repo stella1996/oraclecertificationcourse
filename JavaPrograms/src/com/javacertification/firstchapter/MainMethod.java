6e2d9ef2f6ef930e1873d0fab67b490e806585ce
stella1996 <Admin@Admin-PC.mshome.net>
2017-07-20 19:53:47 +0530

    second commit

---
+++ JavaPrograms/src/com/javacertification/firstchapter/MainMethod.java
@@ -0,0 +1,11 @@
+package com.javacertification.firstchapter;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		System.out.println("stella");
+
+	}
+
+}
+// It doesn't allow me to run if I removed brackets from main method;
